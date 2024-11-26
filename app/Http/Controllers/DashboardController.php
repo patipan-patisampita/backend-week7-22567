5fe177682fe805493c27597dd7591995eea3aa9e
patipan-patisampita <patipan-patisampita@gmail.com>
2024-11-26 11:26:20 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index() {
-        return view('admin.dashboard');
+    public function index()
+    {
+        return view('dashboard', ['title' => 'Account Dashboard']);
     }
 }
